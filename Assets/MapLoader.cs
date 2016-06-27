35a0a2393cf83e8af401d47cb199b5c00f207ca1
Bryan Duggan <skooter500@gmail.com>
2016-06-27 16:46:45 +0100

    Map v1

--- Assets/MapLoader.cs
+++ Assets/MapLoader.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MapLoader : MonoBehaviour {
 
 	// Use this for initialization
 	IEnumerator Start () {
-        string lat = "56";
-        string lon = "-6";
-        //string url = "http://maps.google.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=14&size=800x600&type=hybrid&sensor=true";
-        string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+        string lat = "53.344";
+        string lon = "-6.287";
+        string url = "http://maps.google.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=14&size=300x300&type=hybrid&sensor=true";
         WWW www;
         www = new WWW(url);
         yield return www;
-        GetComponent<Renderer>().material.mainTexture = www.texture;
+        RawImage ri = GetComponent<RawImage>();
+        ri.texture = www.texture;        
     }
 	
 	// Update is called once per frame
