b6fddef8177ed3b0cfe0457b5fb4c92f11c3f6e6
Bryan Duggan <skooter500@gmail.com>
2016-06-30 11:50:37 +0100

    rearrange ui

--- Assets/MapLoader.cs
+++ Assets/MapLoader.cs
@@ -5,18 +5,43 @@ using ZenFulcrum.EmbeddedBrowser;
 
 public class MapLoader : MonoBehaviour {
 
+    public Dropdown dropdown;
+
+    private string MapType 
+    {
+    get
+        {
+            if (dropdown == null)
+            {
+                return "roadmap";
+            }
+            else
+            {
+                return dropdown.options[dropdown.value].text.ToLower();
+            }
+        }
+    }
+
     public void LoadDefaultMap()
     {
-        //StartCoroutine("LoadMap");
         Browser browser = GetComponent<Browser>();
 
         string lat = "53+20 40.55";
         string lon = "-6+16 2.05";
-
+        
         BNOController bno = FindObjectOfType<BNOController>();
         string url = "https://maps.google.com/maps?q=" + lat + "," + lon
-            + "(DN_13206)&iwloc=A&hl=en&output=embed";
+            + "(DN_13206)&iwloc=A&hl=en&zoom=1&output=embed&maptype=" + MapType;
+        /*
+        https://www.google.com/maps/embed/v1/view
+  ? key = YOUR_API_KEY
+  & center = -33.8569,151.2152
+     & zoom = 18
+     & maptype = satellite
 
+
+        AIzaSyCnLmacv0vthhAh4vLsUkQs - 7CC_7i8mGI
+        */
         //browser.Url = url;
         //string html = "<html><body>Hello</body></html>";
 
@@ -26,15 +51,13 @@ public class MapLoader : MonoBehaviour {
         Debug.Log(url);
     }
 
-
-
     public void LoadMap(string param = "")
     {
         Browser browser = GetComponent<Browser>();
 
         BNOController bno = FindObjectOfType<BNOController>();
                 string url = "https://maps.google.com/maps?q=" + bno.latitude + "," + bno.longitude
-            + "(DN_13206)&iwloc=A&hl=en&output=embed";
+            + "(DN_13206)&iwloc=A&hl=en&output=embed&maptype=" + MapType;
 
         string html = "<blah><head><style type=\"text/css\">body {scrolling:no;}iframe {position:absolute;z-index:1;   top:0px;    left:0px;}</style></head><body><iframe src=\"" + url +  "\" height=\"100%\" width=\"100%\" frameborder=\"0\"></iframe></body>";
 
